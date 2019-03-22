77a7792c64811b3b66e03c518e7e32b0e6858938
manzilat <amtulnoorq@yahoo.com>
2019-03-21 22:11:56 -0500

    created category action for shop shop controller

--- ShoppingCart/ShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ ShoppingCart/ShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShoppingCart.Models.Data;
+using ShoppingCart.Models.ViewModels.Shop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,24 @@ namespace ShoppingCart.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop
-        public ActionResult Index()
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
         {
-            return View();
+            //Declre list of models
+            List<CategoryVM> categoryVMList;
+            using (Db db = new Db())
+            {
+                //init list
+                categoryVMList = db.Categories
+                    .ToArray().
+                    OrderBy(x => x.Sorting)
+                    .Select(x => new CategoryVM(x))
+                    .ToList();
+            }
+
+
+            //return view with list
+            return View(categoryVMList);
         }
     }
 }
--- ShoppingCart/ShoppingCart/Models/ViewModels/Shop/CategoryVM.cs
+++ ShoppingCart/ShoppingCart/Models/ViewModels/Shop/CategoryVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShoppingCart.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,21 @@ namespace ShoppingCart.Models.ViewModels.Shop
 {
     public class CategoryVM
     {
+        public CategoryVM()
+        {
+
+        }
+        public CategoryVM(CategoryDTO row)
+        {
+            Id = row.Id;
+            Name = row.Name;
+            Slug = row.Slug;
+            Sorting = row.Sorting;
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int Sorting { get; set; }
     }
 }
